9b587dd76159b3d514f0df586a04cfd236895c5f
kowf <kathy.kowf@gmail.com>
2019-08-15 18:53:35 +0800

    fix high score list ranking

--- components/Score.js
+++ components/Score.js
@@ -9,22 +9,27 @@ export default class Score extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
-      list: []
+      list: [],
+      score: []
     }
   }
 
 
-  componentWillMount() {
-    this.load()
+  componentDidMount() {
+    this.load();
   }
 
   load = async () => {
     try {
       const list = await AsyncStorage.getItem('HIGHSCORE')
       if (list !== null) {
-        let parsed = JSON.parse(list)
-        this.setState({ list: parsed })
-
+        let parsed = JSON.parse(list);
+        this.setState({ list: parsed });
+        const score = parsed.map(item => item.score)
+        const distinctScore = [...new Set(score)];
+        distinctScore.sort((a, b) => b - a)
+        this.setState({ score: distinctScore });
+        this.render();
       }
     } catch (e) {
       console.error('Failed to load score list.')
@@ -32,26 +37,26 @@ export default class Score extends React.Component {
   }
 
   render() {
-    
-    for (let i = 0; i < this.state.list.length; i++) {
-    }
-    const renderScore = this.state.list.map((item, i) =>
-      (
+    const renderScore = this.state.list.map((item, i) => {
+      if (i > 10) { 
+        return null; 
+      }
+      return (
         <View key={i} style={styles.row}>
-          <Text>{item.rank}</Text>
+          <Text>{this.state.score.findIndex(v => v === item.score) + 1}</Text>
           <Text>{item.name}</Text>
           <Text>{item.score}</Text>
         </View>
       )
-    )
-    return (
-      <View style={styles.container}>
-        <View style={styles.row}>
-          <Text>Rank</Text>
-          <Text>Name</Text>
-          <Text>Score</Text>
-        </View>
-        {renderScore}
+  })
+  return(
+      <View style = { styles.container } >
+      <View style={styles.row}>
+        <Text>Rank</Text>
+        <Text>Name</Text>
+        <Text>Score</Text>
+      </View>
+        { renderScore }
       </View>
 
     );
